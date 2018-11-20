e77ba54b5bf80d7eab361e0a7b30e63c806b6864
nuccelsa <elsa.fredenucci@gmail.com>
2018-11-20 15:33:11 +0100

    more details to come

--- app/initialize.js
+++ app/initialize.js
@@ -2,57 +2,54 @@ var app = {
 
   initialize: function () {
     $('#button-character').on('click', app.showCharacters);
-    $('#button-ships').on('click', app.showShips);
   },
 
   showCharacters: function (event) {
 
     $.ajax ("https://swapi.co/api/people/?format=json", {
-      success: function (datas){
+      success: function (datas){ 
         // console.log(data.results[0].name);
         var data = datas.results;
-          
+
         for (var index = 0; index < data.length ; index++) {
           var characterNames = data[index].name;
           var ul = $('.list-names');
-          var li = $('<li>');
+          var li = $(`<li id="name-`+ index + `">`);
+          var html = 
+              `<ul>
+                  <li>
+                      Couleurs des yeux :`
+                      + data[index].eye_color +
+                  `</li>
+                  <li>
+                      Couleurs des cheveux :`
+                      + data[index].hair_color +
+                  `</li>
+                  <li>
+                      Taille :`
+                      + data[index].height +
+                  `</li>
+                  <li>
+                      Poids :`
+                      + data[index].mass +
+                  `</li>
+              </ul>`;
+              console.log(html);
           
-          ul.append(li);
           li.text(characterNames).addClass('name');
+          ul.append(li);
+
+         li.on('click', showMore());
+
+         function showMore(evt){
+          li.append(html);
+         }
+        
         }  
 
-        }
-      })
-
-    },
-
-  showDetails: function (event) {
-    var html = `<ul>
-                   <li>
-                       Couleurs des yeux :`
-                        + data.results[index].eye_color +
-                   `</li>
-                    <li>
-                       Couleurs des cheveux :`
-                        + data.results[index].hair_color +
-                   `</li>
-                   <li>
-                       Taille :`
-                        + data.results[index].height +
-                   `</li>
-                   <li>
-                       Poids :`
-                        + data.results[index].mass +
-                   `</li>
-                </ul>`;
-
-  }
-
-/* showShips: function (event) {
-
-} */
-
-  
-}
+      } // success
+    }) // ajax
+  } // show
+} // app
 
 $(app.initialize);
