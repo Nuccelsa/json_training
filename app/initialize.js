5343e8d7d456b8575bf17b09ecab1fd88e03c1fc
nuccelsa <elsa.fredenucci@gmail.com>
2018-11-21 15:54:49 +0100

    add characteristics for each characters

--- app/initialize.js
+++ app/initialize.js
@@ -8,48 +8,47 @@ var app = {
 
     $.ajax ("https://swapi.co/api/people/?format=json", {
       success: function (datas){ 
-        // console.log(data.results[0].name);
+  
         var data = datas.results;
 
         for (var index = 0; index < data.length ; index++) {
           var characterNames = data[index].name;
           var ul = $('.list-names');
           var li = $(`<li id="name-`+ index + `">`);
+          li.text(characterNames).addClass('name');
+          ul.append(li);
+          
           var html = 
               `<ul>
                   <li>
-                      Couleurs des yeux :`
+                      Eye color : `
                       + data[index].eye_color +
                   `</li>
                   <li>
-                      Couleurs des cheveux :`
+                      Hair color : `
                       + data[index].hair_color +
                   `</li>
                   <li>
-                      Taille :`
-                      + data[index].height +
-                  `</li>
-                  <li>
-                      Poids :`
-                      + data[index].mass +
+                      Birthday : `
+                      + data[index].birth_year +
                   `</li>
               </ul>`;
-              console.log(html);
-          
-          li.text(characterNames).addClass('name');
-          ul.append(li);
-
-         li.on('click', showMore());
-
-         function showMore(evt){
           li.append(html);
-         }
-        
-        }  
 
+          $('.name').children().css('display', 'none');
+        }  
+        $('.name').on('click', app.showMore);
+    
       } // success
     }) // ajax
-  } // show
+  }, // showCharacters
+
+  showMore: function (evt) {
+    $(this).children().css('display', 'block');
+    $(this).on('click', function(){
+        $(this).children().hide();
+    })
+  }
 } // app
 
 $(app.initialize);
